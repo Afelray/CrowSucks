c931bd203e432802ca0009da5a0600825e48c1b3
Afelray <rafel_peh_zheng_yi@s2024.ssts.edu.sg>
2024-09-09 16:05:32 +0800

    Initial Commit

---
+++ GitHub (CrowSucks)/GitHub__CrowSucks_App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub__CrowSucks_App.swift
+//  GitHub (CrowSucks)
+//
+//  Created by Rafel Peh Zheng Yi on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub__CrowSucks_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
